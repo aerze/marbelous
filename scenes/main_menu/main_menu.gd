02ff038748cac1f8e160054fe369c0b20e04ec7d
Abby Suarez <abby@mythril.co>
2024-02-29 22:13:15 -0800

    fixed broken collision mask value

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -4,7 +4,6 @@ var marble_game: PackedScene = preload("res://scenes/game/marble_game.tscn");
 var gacha_shop: PackedScene = preload("res://scenes/gacha_shop/gacha_shop.tscn");
 
 func _on_play_button_pressed() -> void:
-	print_debug("pressed");
 	get_tree().change_scene_to_packed(marble_game);
 	return;
 
@@ -12,10 +11,8 @@ func _on_shop_button_pressed() -> void:
 	get_tree().change_scene_to_packed(gacha_shop);
 	return;
 
-
 func _on_settings_button_pressed() -> void:
 	Global.playerData.wallet = 100;
 	Global.playerData.roll_count = 5;
 	Global.saveGame();
 	return;
-
